e6db484ec106e4342e27326baa0c94f34f5093dc
daniii808 <rappdaniel2005@gmail.com>
2025-01-08 00:48:40 +0100

    hinzufügen weiterer Hotels und auswählen bei start

--- src/main/java/org/example/Hotel.java
+++ src/main/java/org/example/Hotel.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import javax.swing.*;
 import java.util.ArrayList;
 
 public class Hotel {
@@ -8,7 +7,7 @@ public class Hotel {
     public double HotelPreis;
     public boolean AllInclusive;
 
-    public Hotel(){
+    public Hotel(String auswählen){
         initObjekte();
     }
     public Hotel (String Hotelnamen, double HotelPreis, boolean AllIclusive){
@@ -25,9 +24,15 @@ public class Hotel {
     public static ArrayList<Hotel> getHotels(){
         ArrayList<Hotel> hotels = new ArrayList<>();
 //3 Objekte erstellt mit den Attributen Hotelnamen, Hotelpreis und AllInclusive(Später noch in der Klasse Reiseplanung.java Datei den Hotelpreis in den GesamtPreis hinzufügen)
+        hotels.add(new Hotel("--Auswählen--", 0.0, false));
         hotels.add(new Hotel("Mercy Hotel", 55.99, true));
         hotels.add(new Hotel("Olive Hotel", 20.99, false));
         hotels.add(new Hotel("Modern Hotel", 60.59, true));
+        hotels.add(new Hotel("Sunrise Paradise", 45.50, true));
+        hotels.add(new Hotel("Grand Elite", 75.00, true));
+        hotels.add(new Hotel("Ocean Breeze", 30.25, false));
+        hotels.add(new Hotel("Cityscape Inn", 40.00, true));
+        hotels.add(new Hotel("Mountain Retreat", 50.75, false));
         return hotels;
     }
 
